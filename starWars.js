bccc31f176e4c137213d24ef73678fdcd2a157a3
pilillani <lorenia.jimenez@gmail.com>
2017-07-06 20:42:11 -0700

    if no more enemies game over

--- starWars.js
+++ starWars.js
@@ -5,14 +5,17 @@ $(document).ready(function(){
 		var darthMaul = 180;
 		var darthSidious = 150;
 		var opponent;
+		var defender;
 		var player;
+		var yourPlayer;
 		var playerHealthPower;
 		var opponentHealthPower;
 		var playerAttack;
 		var opponentAttack;
+		$("#restartGame").hide();
 
 		$("#characters div").on("click", function(){
-			var yourPlayer = (this.id);
+			yourPlayer = (this.id);
 			$(("#" + yourPlayer)).appendTo($("#player-character"));
 			player = $("#player-character").children("#" + yourPlayer).text();
 			var myPlayer = $.trim(player)
@@ -38,7 +41,8 @@ $(document).ready(function(){
 
 			$("#enemies-available div").off("click");
 			$("#enemies-available div").on("click", function(){
-				var defender = (this.id);;
+				$("#game-status").empty();
+				defender = (this.id);;
 				$(("#" + defender)).appendTo($("#defender"));
 				opponent = $("div").children("#" + defender).text();
 				console.log(opponent);
@@ -62,14 +66,23 @@ $(document).ready(function(){
 			});
 			// $("#defender div").off("click");
 			$("#attack").click(function(){
-				$("#game-status").empty();
-				attack();
+				if($("#" + defender).length){
+					$("#game-status").empty();
+					attack();
+				}else{
+					$("#game-status").text("No enemy has been chosen.");
+				}
+			});
 
+			$("#restartGame").click(function(){
+				console.log("this button works!")
+				location.reload(true);
 			});
 
 		});
 
 		function attack(){
+			console.log(playerAttack);
 			$("#game-status").append("You attacked " + opponent + " for " + playerAttack + " damage."); 
 			$("#game-status").append("<div>" + opponent + "attacked you" + " for " + opponentAttack + " damage.</div>");
 
@@ -81,47 +94,29 @@ $(document).ready(function(){
 
 			if(playerHealthPower > 0 & opponentHealthPower > 0){
 				$("#game-status").empty();
-				playerAttack = playerAttack + playerAttack;
-				playerHealthPower = playerHealthPower - opponentAttack;
-				opponentHealthPower = opponentHealthPower - playerAttack;
 
 				$("#game-status").append("You attacked " + opponent + " for " + playerAttack + " damage."); 
 				$("#game-status").append("<div>" + opponent + "attacked you" + " for " + opponentAttack + " damage.</div>");
-			}else if(playerHealthPower <= 0 & opponentHealthPower > 0){
-				$("#game-status").empty();
-				$("#game-status").append("You lose!");
-				$("#game-status").append('</br><button id="restartGame">Restart</button>');
-			}else if(opponentHealthPower <= 0 & playerHealthPower > 0){
-				$("#game-status").empty();
-				$("#game-status").append("You win!");
-
+				playerAttack = playerAttack + playerAttack;
+				playerHealthPower = playerHealthPower - opponentAttack;
+				opponentHealthPower = opponentHealthPower - playerAttack;
 			}
-
-		}
+			else if(playerHealthPower <= 0 & opponentHealthPower > 0){
+				$("#game-status").empty();
+				$("#game-status").append("You have been defeated. GAME OVER!!!");
+				$("#restartGame").show();
+			}
+			else if(opponentHealthPower <= 0 & playerHealthPower > 0){
+				$("#game-status").empty();
+				$("#" + defender).remove();
+				if ($("#enemies-available").children().length > 1 ) {
+					$("#game-status").append("You have defeated " + opponent + ". You can choose to fight another enemy.");
+				}else{
+					$("#game-status").text("YOU HAVE DEFEATED ALL ENEMIES! YOU WIN!!!");
+					$("#restartGame").show();
+				}
+			}
 		
-		// function getDefender(){
-		// 	var defender = (this.id);;
-		// 		$(("#" + defender)).appendTo($("#defender"));
-		// 		opponent = $("div").children("#" + defender).text();
-		// 		console.log(opponent);
-		// 		var myOpponent = $.trim(opponent)
-		// 		console.log(myOpponent);
-		// 		if(myOpponent === "Luke Skywalker"){
-		// 			opponentHealthPower = lukeSkywalker;
-		// 			opponentAttack = 5;
-		// 		}else if(myOpponent === "Obi-Wan"){
-		// 			opponentHealthPower = obiWan;
-		// 			opponentAttack = 8;
-		// 		}else if(myOpponent === "Darth Maul"){
-		// 			opponentHealthPower = darthMaul;
-		// 			opponentAttack = 20;
-		// 		}else if(myOpponent === "Darth Sidious"){
-		// 			opponentHealthPower = darthSidious;
-		// 			opponentAttack = 25;
-		// 		}
-		// 	$("#p-health-power").text("Your Health Power: " + playerHealthPower);
-		// 	$("#o-health-power").text(opponent + "Health Power: " + opponentHealthPower);
-		// 	});
-		// }
+		}
 
 });
